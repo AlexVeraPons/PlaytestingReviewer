ea4ad7a17cbe83699a6456abbbbd0be3749eb77b
AlexVeraPons <107070295+AlexVeraPons@users.noreply.github.com>
2025-01-16 20:34:29 +0100

    Revert "zooming in achieved"
    This reverts commit 2282c791a97a3eea556b3b1e27125fead5f7040e.

--- Assets/_Project/Editor/TimeIndicatorController.cs
+++ Assets/_Project/Editor/TimeIndicatorController.cs
@@ -7,23 +7,18 @@ namespace PlaytestingReviewer.Editor
 {
     public class TimeIndicatorController
     {
-        private const int MinimumSpaceBetweenIndicator = 10;
         private const int InitialSpaceBetweenIndicators = 10;
-        private const int MaxSpaceBeforeSplit = 50;
+        private const int MaxSpaceBetweenIndicators = 20;
         private const float LabelSize = 20f;
 
         private List<Label> _timeIndicators;
         private ScrollView timeScroll;
         private VisualElement timeView;
-
-
+        
+        
         private float _currentSpaceBetweenIndicators = 10f;
         private float _videoLength;
-
-        private float _currentZoom = 0f;
-        private const float MaxZoom = 8f;
-        private const float MinZoom = -8f;
-
+        
 
 
         public TimeIndicatorController(VisualElement root)
@@ -43,14 +38,8 @@ namespace PlaytestingReviewer.Editor
         {
             _videoLength = videoLength;
             _timeIndicators = new List<Label>();
-            
-            float trackViewWidth = timeView.layout.width;
-
-
+            float trackViewWidth = timeScroll.resolvedStyle.width;
             int indicatorCount = Mathf.FloorToInt(trackViewWidth / (InitialSpaceBetweenIndicators + LabelSize));
-            _currentSpaceBetweenIndicators = InitialSpaceBetweenIndicators;
-
-            timeView.Clear();
 
             for (int i = 0; i < indicatorCount; i++)
             {
@@ -92,25 +81,11 @@ namespace PlaytestingReviewer.Editor
             if (evt.ctrlKey)
             {
                 ZoomTimeIndicators(evt.delta.y);
-
-                if (_currentSpaceBetweenIndicators > MaxSpaceBeforeSplit)
-                {
-                    ReloadIndicators(_videoLength);
-                }
-
-                if(_currentSpaceBetweenIndicators < MinimumSpaceBetweenIndicator)
-                {
-                    ReloadIndicators(_videoLength);
-                }
             }
         }
 
         private void ZoomTimeIndicators(float amount)
         {
-            Debug.Log(_currentZoom);
-            _currentZoom = Mathf.Clamp(_currentZoom + amount * 0.1f, MinZoom, MaxZoom);
-
-
             _currentSpaceBetweenIndicators += amount;
             foreach (VisualElement indicator in _timeIndicators)
             {
